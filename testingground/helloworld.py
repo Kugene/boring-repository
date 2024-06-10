d717c6ef8dc43ef0cbdb1a7c1a8612eaad2c4c83
Eugene Kim <kim1948@purdue.edu>
2024-06-10 00:16:19 -0400

    fifth commit

--- testingground/helloworld.py
+++ testingground/helloworld.py
@@ -1,5 +1,10 @@
+# git add .
+# git status
+# git 
+#
 
 
 print("hello world")
 print("I think this git is working")
-print ("I think this is also working") # I think this works well!
+print("I think this is also working") # I think this works well!
+print("Lets see if I am able to share this with my Surface!")
